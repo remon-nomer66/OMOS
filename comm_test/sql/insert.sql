660c018d6fcc6b55c5f32fe68be51e13cf75d339
otomato2 <yukiyoshi0001U@gmail.com>
2023-07-07 18:49:47 +0900

    changed command test files

---
+++ comm_test/sql/insert.sql
@@ -0,0 +1,110 @@
+BEGIN TRANSACTION;
+
+INSERT INTO user_t (
+    user_id, user_phone, user_name, user_pass
+)
+VALUES (
+    1001, 03012345678, '田中 太郎', 'qwerty1234'
+);
+
+INSERT INTO user_point_t (
+    user_id, user_point, user_mag
+)
+VALUES (
+    1001, 100, 1.5
+);
+
+INSERT INTO user_authority_t (
+    user_id, user_authority, store_id
+)
+VALUES (
+    1001, 5, 10
+);
+
+INSERT INTO store_t (
+    store_id, store_name
+)
+VALUES (
+    10, 'IZAKAYA'
+);
+
+INSERT INTO region_t (
+    store_id, region_id, region_name
+)
+VALUES (
+    10, 2, '宮崎'
+);
+
+INSERT INTO store_table_t (
+    store_id, desk_num, desk_max, desk_use
+)
+VALUES (
+    10, 2, 10, 0
+);
+
+INSERT INTO order_t (
+    store_id, desk_num, menu_id, order_cnt, kitchen_flag, order_date, order_time, user_id
+)
+VALUES (
+    1, 2, 1234, 5, 1, '2023-06-30', '12:34:00', 1001
+);
+
+INSERT INTO summary_t (
+    store_id, menu_id, order_cnt, order_date, order_time, user_id
+)
+VALUES (
+    1, 1234, 5, '2023-06-30', '12:34:00', 1001
+);
+
+INSERT INTO recipe_t (
+    menu_id, menu_name, recipe
+)
+VALUES (
+    1234, 'たこわさ', 'たこを切って，わさびを和えます'
+);
+
+INSERT INTO menu_price_t (
+    menu_id, price
+)
+VALUES (
+    1234, 300
+);
+
+INSERT INTO menu_charge_t (
+    menu_id, user_id
+)
+VALUES (
+    1234, 1001
+);
+
+INSERT INTO push_t (
+    menu_id, push_hq, push_cor, push_mgr, layer
+)
+VALUES (
+    1234, 1, 0, 1, 3
+);
+
+INSERT INTO menu_storage_t (
+    menu_id, store_id, storage, min_storage
+)
+VALUES (
+    1234, 1, 100, 30
+);
+
+SET search_path to reserve;
+INSERT INTO reserve_t (
+    reserve_no, user_id, people_num, reserve_date, reserve_time, store_id, desk_num
+)
+VALUES (
+    nextval('reserve_seq'), 1001, 8, '2023-08-23', '12:30:00', 1, 2
+);
+SET search_path to public;
+
+INSERT INTO store_order_t (
+    store_id, menu_id, store_order_cnt, store_order_date, store_order_time
+)
+VALUES (
+    1, 1234, 200,  '2023-06-30', '11:34:21'
+);
+
+COMMIT;
--- comm_test/test_func.c
+++ comm_test/test_func.c
@@ -1,33 +1,43 @@
+#include "omos.h"
 #include "test.h"
 
-void test_func(PGconn *__con, int __soc){
-    char sendBuf[BUFSIZE], recvBuf[BUFSIZE];
+int test_func(pthread_t selfId, PGconn *con, int soc, char *recvBuf, char *sendBuf){
     int sendLen, recvLen;
     int param1, param2;
     int cnt;
-    pthread_t selfId;
-
-    selfId = pthread_self();
+    char buf[BUFSIZE];
 
     while(1){
-        sprintf(sendBuf, "足し合わせたい2つの数値を「1 23」の形で入力してください．%s", ENTER);
+        sprintf(sendBuf, "足し合わせたい2つの数値を「1 23」の形で入力してください%s", ENTER);
         sendLen = strlen(sendBuf);
-        send(__soc, sendBuf, sendLen, 0);
-        printf("[C_THREAD %ld] SEND=> %s\n", selfId, sendBuf);
-        recvLen = receive_message(__soc, recvBuf, BUFSIZE);
+        send(soc, sendBuf, sendLen, 0);
+        printf("[C_THREAD %ld] SEND=> %s", selfId, sendBuf);
+
+	sendLen = sprintf(sendBuf, "%s", DATA_END);
+	send(soc, sendBuf, sendLen, 0);
+	printf("[C_THREAD %ld] SEND=> %s\n", selfId, sendBuf);
+        
+        recvLen = receive_message(soc, recvBuf, BUFSIZE);
         if(recvLen > 0){
             recvBuf[recvLen - 1] = '\0';
             printf("[C_THREAD %ld] RECV=> %s\n", selfId, recvBuf);
             cnt = sscanf(recvBuf, "%d %d", &param1, &param2);
             if(cnt == 2){
                 param1 += param2;
-                sprintf(sendBuf, "結果: %d%s", param1, ENTER);
+                sprintf(sendBuf, "結果: %d%s%s", param1, ENTER, DATA_END);
                 sendLen = strlen(sendBuf);
-                send(__soc, sendBuf, sendLen, 0);
+                send(soc, sendBuf, sendLen, 0);
                 printf("[C_THREAD %ld] SEND=> %s\n", selfId, sendBuf);
                 break;
             }
         }
+        if(recvLen == 0 || cnt != 2){
+	  sprintf(sendBuf, "%s %d%s%s", ER_STAT, E_CODE_1001, ENTER, DATA_END);
+            sendLen = strlen(sendBuf);
+            send(soc, sendBuf, sendLen, 0);
+            printf("[C_THREAD %ld] SEND=> %s\n", selfId, sendBuf);
+            return -1;
+        }
     }
-    return;
+    return 0;
 }
