a3c59ac8ebe51ddeee2821bda04e2a5f74237457
TakumiMura <hn21338@student.miyazaki-u.ac.jp>
2023-07-24 19:01:38 +0900

    add kitchen.h

---
+++ base/headerFiles/kitchen.h
@@ -0,0 +1,16 @@
+/**
+ * kitchen.h
+ **/
+
+#ifndef _INCLUDE_KITCHEN_FUNC_
+#define _INCLUDE_KITCHEN_FUNC_
+
+//*** エラーコード ***//
+#define E_CODE_1501 1501 // 注文されたメニューがない
+#define E_CODE_1502 1502 // キッチンに残っているメニューがあり、削除できない
+
+#define E_CODE_1503 1503 // IDエラー
+
+#define E_CODE_1504 1504 // フラグを１にできない
+
+#endif
